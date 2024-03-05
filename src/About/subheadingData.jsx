c3a53f931fefe1e3687917c019da318982943b06
AryanPardhi <aryanpardhi2002@gmail.com>
2024-03-05 19:57:31 +0530

    remove dummy data

--- src/About/subheadingData.jsx
+++ src/About/subheadingData.jsx
@@ -1,57 +1,82 @@
+import "../Styles/aboutMenu.css";
 const subheadingData = {
-    1:[
-        {
-            title : "About Me",
-            content : (
-                <>
-                    <p>lorem10</p>
-                </>
-            ),
-        },
-    ],
-    2:[
-        {
-            title : "2020 High School",
-            content : (
-                <>
-                    <p>lorem10</p>
-                </>
-            ),
-        },
-        {
-            title : "2024 Bachelors Degree",
-            content : (
-                <>
-                    <p>lorem10</p>
-                </>
-            ),
-        },
-    ],
-    3:[
-        {
-            title : "Front End Developer",
-            content : (
-                <>
-                    <p>lorem10</p>
-                </>
-            ),
-        },
-        {
-            title : "Mern Stack",
-            content : (
-                <>
-                    <p>lorem10</p>
-                </>
-            ),
-        },
-        {
-            title : "Content Creator",
-            content : (
-                <>
-                    <p>lorem10</p>
-                </>
-            ),
-        },
-    ]
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <div className="about-scroll">
+            <p>
+              Motivated and enthusiastic fresher aspiring to kickstart a career
+              as a MERN Stack Developer. Proficient in developing web
+              applications using the MERN technology stack. Skilled in
+              implementing Data Structures and Algorithms (DSA) with Java.
+              Possesses a strong foundation in software development principles
+              and a passion for learning and growing within the tech industry.
+              Demonstrated capability through individual projects showcasing
+              hands-on experience with MERN stack development and Java-based
+              DSA. Ready to contribute innovative solutions and collaborate
+              effectively in a dynamic team environment.
+            </p>
+          </div>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "2020 High School",
+      content: (
+        <>
+          <p>B.E.H.S.S CBSE</p>
+        </>
+      ),
+    },
+    {
+      title: "2024 Bachelors Degree",
+      content: (
+        <>
+          <p>IPS Academy Indore</p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    // {
+    //   title: "Front End Developer",
+    //   content: (
+    //     <>
+    //       <p>lorem10</p>
+    //     </>
+    //   ),
+    // },
+    {
+      title: "Mern Stack Developer",
+      content: (
+        <>
+          <div className="about-scroll">
+            <p>
+              Empowering Businesses through Innovative MERN Stack Solutions:
+              Crafting dynamic web applications with MongoDB, Express.js,
+              React.js, and Node.js to drive digital transformation and enhance
+              user experiences.
+            </p>
+          </div>
+        </>
+      ),
+    },
+    {
+      title: "Content Creator",
+      content: (
+        <>
+          <div className="about-scroll">
+            <p>
+            "Empowering Audiences: Crafting Compelling Content on YouTube"
+            </p>
+          </div>
+        </>
+      ),
+    },
+  ],
 };
-export default subheadingData
+export default subheadingData;
--- src/Projects/projectsData.jsx
+++ src/Projects/projectsData.jsx
@@ -1,41 +1,61 @@
-import projectOne from "../assets/project-1.png"
-import projectTwo from "../assets/project-2.png"
-import projectThree from "../assets/project-3.png"
+import projectOne from "../assets/project-1.png";
+import projectTwo from "../assets/project-2.png";
+import projectThree from "../assets/project-3.png";
+import "../Styles/projectsMenu.css"
+
 
 const projects = {
-    1:{
-        title : "social",
-        image : projectOne,
-        description : (
-            <>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolorum!</p>
-            </>
-        ),
-        github: "http",
-        Demo: "http",
-    },
-    2:{
-        title : "social",
-        image : projectTwo,
-        description : (
-            <>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolorum!</p>
-            </>
-        ),
-        github: "http",
-        Demo: "http",
-    },
-    3:{
-        title : "social",
-        image : projectThree,
-        description : (
-            <>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolorum!</p>
-            </>
-        ),
-        github: "http",
-        demo: "http",
-    },
+  1: {
+    title: "Placify",
+    image: projectOne,
+    description: (
+      <>
+        <div className="projects-desc-scroll">
+          <p>
+            Placify streamlines the campus placement process, offering a
+            user-friendly web app where students can directly apply for job
+            postings from registered companies. With intuitive features,
+            recruiters can efficiently manage applications, accepting or
+            rejecting candidates based on specific requirements. Placify ensures
+            a smoother, more convenient placement experience for both students
+            and recruiters alike.
+          </p>
+        </div>
+      </>
+    ),
+    github: "https://github.com/AryanPardhi/PLacify.git",
+    Demo: "http",
+  },
+  2: {
+    title: "vNotes",
+    image: projectTwo,
+    description: (
+      <>
+        <div className="projects-desc-scroll">
+          <p>
+          vNotes: Your Secure Web App for Organizing All Your Notes in One Place. With top-notch security measures, vNotes ensures that only you have access to your notes, providing peace of mind while keeping your information organized and easily accessible.
+          </p>
+        </div>
+      </>
+    ),
+    github: "https://github.com/AryanPardhi/vNotes.git",
+    Demo: "http",
+  },
+  3: {
+    title: "NewseMon",
+    image: projectThree,
+    description: (
+      <>
+        <div className="projects-desc-scroll">
+          <p>
+          NewseMon: Your Personalized Web App for Staying Updated with the Latest News Across the Country. With a dynamic news feed feature, NewsEmon delivers real-time updates on current events, ensuring you're always in the know about what's happening nationwide.
+          </p>
+        </div>
+      </>
+    ),
+    github: "https://github.com/AryanPardhi/NewseMon.git",
+    demo: "http",
+  },
 };
 
 export default projects;
